e157cd18196d280ebba9b8c934ad27d84bae6a77
JakeUGT <swc03218@gmail.com>
2023-01-09 16:41:46 +0900

    Feat : added print(anwser) for debugging

--- numguess.py
+++ numguess.py
@@ -2,9 +2,12 @@ from random import randint
 
 answer = randint(1,100)
 
+##for debugging
+print(answer)
+
 username = input("Name please? ")
 guess = int(input(f"So {username}, what is your choice?: "))
 if guess is answer:
   print("Excellent Choice! You got it!")
 else :
-  print("hmm I don't think so mate")
+  print("hmm I don't think so mate, try again")
