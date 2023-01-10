966a7466356c534bb44e5374beae14641f7d6a1f
JakeUGT <swc03218@gmail.com>
2023-01-10 15:15:54 +0900

    feat : added hint

--- numguess.py
+++ numguess.py
@@ -1,4 +1,5 @@
 from random import randint
+from time import sleep
 
 answer = randint(1,100)
 
@@ -7,7 +8,22 @@ print(answer)
 
 username = input("Name please? ")
 guess = int(input(f"So {username}, what is your choice?: "))
-if guess is answer:
-  print("Excellent Choice! You got it!")
-else :
-  print("hmm I don't think so mate, try again")
+
+#compare answer with user's guess
+
+
+
+if guess > ans:
+  hint= ("hint : ↓")
+elif guess < ans:
+  hint = ("hint : ↑")
+
+if guess == answer:
+    print("************************")
+    sleep(1)
+    print("************************")
+    sleep(1)
+    print("Excellent Choice! You got it!")
+elif guess != answer:
+    print(hint, "ERGGGGGGGG!!!!!!! YOU STUPID"
+
