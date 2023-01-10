15151b04f9490b26f96f17641c01d35cddfea5d7
JakeUGT <swc03218@gmail.com>
2023-01-10 15:21:47 +0900

    feat: Used while loop. Keep asking till you get it

---
+++ numguessver2.py
@@ -0,0 +1,23 @@
+from random import randint
+from time import sleep
+
+ans = randint(1,100)
+print(ans)
+
+username = input("Name please? ")
+guess = int(input(f"So {username}, what will be your choice?: "))
+
+if guess > ans:
+  hint= ("hint :  ")
+elif guess < ans:
+  hint = ("hint : ↑")
+
+while guess != ans:
+  print("Hell no ERGGGGGGGGGGGGGGG")
+  sleep(1)
+  print(hint)
+  guessagain = int(input("One more?"))
+  if guessagain is ans:
+    sleep(1)
+    print("FINALLYY")
+    break
