0b9bc455b0d27c87cdc2d22976792cf026bbd290
JakeUGT <swc03218@gmail.com>
2023-01-11 10:46:36 +0900

    feat: set range(1000) for simulation

--- monty.py
+++ monty.py
@@ -6,12 +6,11 @@ result = {
 }
 
 doors = [0,0,1] #0:goat, 1:car
-shuffle(doors)
-user_choice = choice(doors)
-print(user_choice)
-if user_choice == 0:
-  result ['move_to_win'] += 1
-else:
-  result['stay_to_win'] += 1
-
+for x in range(1000):
+    shuffle(doors)
+    user_choice = choice(doors)
+    if user_choice == 0:
+        result ['move_to_win'] += 1
+    else:
+        result['stay_to_win'] += 1
 print(result)
