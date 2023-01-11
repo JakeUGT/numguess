fd0c33d5b11a4a9f7e10af4893c179f12d124788
JakeUGT <swc03218@gmail.com>
2023-01-11 10:41:41 +0900

    feat: add result(dict) and count for win function

--- monty.py
+++ monty.py
@@ -1,4 +1,17 @@
-from random import shuffle
+from random import shuffle, choice, randint
 
-doors = [0,0,1] #door:0
+result = {
+    'stay_to_win':0,
+    'move_to_win':0,
+}
+
+doors = [0,0,1] #0:goat, 1:car
 shuffle(doors)
+user_choice = choice(doors)
+print(user_choice)
+if user_choice == 0:
+  result ['move_to_win'] += 1
+else:
+  result['stay_to_win'] += 1
+
+print(result)
